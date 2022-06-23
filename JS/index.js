47927ecf39f2b618e7a42cf17861d57b1bc1522f
Ale Godoy <alegodoy03@hotmail.com>
2022-06-23 02:56:42 -0300

    almost

--- JS/index.js
+++ JS/index.js
@@ -11,3 +11,33 @@ button.onclick = () => {
             return conteiner.className = 'contenedor-light'
         }
     }
+
+const inNombre = document.getElementById('nombre');
+const inApellido = document.getElementById('apellido');
+const inEdad = document.getElementById('edad');
+const selPais = document.getElementById('pais');
+console.log(selPais.index)
+
+const div = document.createElement('div');
+
+const main = document.querySelector('#conteiner main')
+
+let formulario = document.getElementById('formulario')
+    formulario.addEventListener("submit", (validar) => {
+        validar.preventDefault();
+        div.innerHTML = `<p>tu nombre es ${inNombre.value} </p>
+        <p>tu apellido es ${inApellido.value}</p>
+        <p> tu edad es ${inEdad.value}</p>
+        <p> naciste en ${selPais.options[selPais.selectedIndex].text}`
+        main.appendChild(div);
+    });
+    
+
+
+// let miFormulario = document.getElementById('formulario');
+// miFormulario.addEventListener('submit', validarFormulario);
+
+// function validarFormulario(e){
+//     e.preventDefault();
+//     console.log('formulario enviado');
+// }
